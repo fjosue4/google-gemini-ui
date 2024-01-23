ce367861c2ce9cef5dac1b812de7e176b8525e86
Franklin Méndez <franklinjosue.net@gmail.com>
2024-01-23 11:52:03 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Welcome from './pages/Welcome/Welcome'
+
+function App() {
+
+  return (
+    <>
+      <Welcome />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Welcome/Welcome.tsx
@@ -0,0 +1,11 @@
+import './Welcome.scss'
+
+function Welcome () {
+    return (
+        <div className='welcome-page'>
+            <h1>Welcome</h1>
+        </div>
+    )
+}
+
+export default Welcome
