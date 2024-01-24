e7271cb2e9a347922dcef5fdf5d69aeaf256fe96
Franklin Méndez <franklinjosue.net@gmail.com>
2024-01-23 18:51:10 -0600

    Add styles for start page

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -2,49 +2,70 @@ import { useState, ChangeEvent } from 'react'
 import './Welcome.scss'
 import { useDispatch } from 'react-redux'
 import { setUser } from '@store/user/userSlice'
+import Button from '@components/Button'
 
 function Welcome() {
-    const [name, setName] = useState<string>('')
-    const [API_KEY, setAPI_KEY] = useState<string>('')
+  const [showInputs, setShowInputs] = useState(false)
+  const [name, setName] = useState<string>('')
+  const [API_KEY, setAPI_KEY] = useState<string>('')
 
-    const dispatch = useDispatch()
-  
-    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-      setName(e.target.value)
-    }
-  
-    const handleApiKeyChange = (e: ChangeEvent<HTMLInputElement>) => {
-      setAPI_KEY(e.target.value)
-    }
-  
-    const handleSubmit = () => {
-      dispatch(setUser({name, API_KEY}))
-    }
-  
-    return (
-      <div className='welcome-page'>
-        <h1>Welcome</h1>
-          <div>
-            <div>Your Name:</div>
-            <input
-              type='text'
-              id='name'
-              value={name}
-              onChange={handleNameChange}
-            />
-          </div>
-          <div>
-            <div>API Key:</div>
-            <input
-              type='text'
-              id='apiKey'
-              value={API_KEY}
-              onChange={handleApiKeyChange}
-            />
-          </div>
-          <button type='submit' onClick={handleSubmit}>Submit</button>
-      </div>
-    )
+  const dispatch = useDispatch()
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value)
   }
-  
-  export default Welcome
+
+  const handleApiKeyChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setAPI_KEY(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    dispatch(setUser({ name, API_KEY }))
+  }
+
+  const getAPI = () => {
+    window.open('https://ai.google.dev/')
+  }
+
+  return (
+    <div className='welcome-page'>
+      <div className='heading'>
+        <h1 className={showInputs ? 'small-h1' : ''}>Chat with <span>Gemini</span> online</h1>
+        {!showInputs ?
+          (
+            <>
+              <h3>Experience Google's largest and most capable AI model</h3>
+              <Button className='start-button' onClick={() => setShowInputs(true)}>Start Setup</Button>
+            </>) :
+          (<div className='auth-container'>
+            <div className='input-container'>
+              <span>Your Name</span>
+              <input
+                type='text'
+                id='name'
+                value={name}
+                onChange={handleNameChange}
+              />
+            </div>
+            <div className='input-container'>
+              <span>API Key</span>
+              <input
+                type='text'
+                id='apiKey'
+                value={API_KEY}
+                onChange={handleApiKeyChange}
+              />
+            </div>
+            <div className='buttons-container'>
+              <Button className='start-button transparent' onClick={getAPI}>Get Free API Key</Button>
+              <Button className='start-button' onClick={handleSubmit}>Setup</Button>
+            </div>
+          </div>)
+        }
+      </div>
+      <span className='disclaimer'>*This is a non-official application</span>
+    </div>
+  )
+}
+
+export default Welcome
