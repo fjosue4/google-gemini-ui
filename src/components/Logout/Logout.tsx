80bc2065c5418bc7c22cce073fbc964e72c518b8
Franklin Méndez <franklinjosue.net@gmail.com>
2024-01-24 10:26:15 -0600

    Update logout

--- src/components/Logout/Logout.tsx
+++ src/components/Logout/Logout.tsx
@@ -1,4 +1,4 @@
-import Button from '@components/Button'
+import Button from '../../components/Button'
 import { clearUser } from '../../store/user/userSlice'
 import { useDispatch } from 'react-redux'
 
